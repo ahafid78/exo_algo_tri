23a6e00718a88f03084ce8991eba360cb40a888d
BEDRI Abdelhafid <ahafid78@gmail.com>
2023-06-15 14:11:18 +0100

    exo_algo_tri

---
+++ script.js
@@ -0,0 +1,16 @@
+function insertionSort(arr) {
+    const n = arr.length;
+    for (let i = 1; i < n; i++) {
+      let key = arr[i];
+      let j = i - 1;
+      while (j >= 0 && arr[j] > key) {
+        arr[j + 1] = arr[j];
+        j--;
+      }
+      arr[j + 1] = key;
+    }
+  }
+  
+  const array = [4, 2, 7, 1, 9, 5];
+insertionSort(array);
+console.log(array);
